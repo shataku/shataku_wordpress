9eed487039ec8d29b76f2bd5ca4938c7ade6451f
matsushimaryouya <54383840+matsushimaryouya@users.noreply.github.com>
2020-03-02 12:59:30 +0900

    Add footer.php

--- wordpress/wp-content/themes/shataku/shataku/footer.php
+++ wordpress/wp-content/themes/shataku/shataku/footer.php
@@ -11,24 +11,9 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'shataku' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'shataku' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'shataku' ), 'shataku', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<footer style="background-color:lightgray;">
+<p>ここはfooter</p>
+</footer>
 
 <?php wp_footer(); ?>
 
